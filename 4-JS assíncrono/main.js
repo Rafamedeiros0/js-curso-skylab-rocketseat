1f53a6e969004a3a6c3d8e88e711ae471cb4add0
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-04-24 20:48:50 -0300

    mod 4 promise

--- 4-JS assíncrono/main.js
+++ 4-JS assíncrono/main.js
@@ -1,10 +1,28 @@
-var xhr = new XMLHttpRequest();
+var minhaPromise = function (){
+  return new Promise(function(resolve,reject) {
+    //resolve quando teve sucesso
+    //reject quando não teve sucesso
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/rafamedeiros0')
+    xhr.send(null);
 
-xhr.open('GET', 'https://api.github.com/users/rafamedeiros0');
-xhr.send(null);
-
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else{
+          reject('Erro na requisição');
+        }
+      }
+    }
+    
+  });
 }
+
+minhaPromise()
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.warn(error);
+  })
